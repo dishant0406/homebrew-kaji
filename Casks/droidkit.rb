65f46185f62005cd73aa53b2ebb90249bfcea034
Dishant <dishu5570@gmail.com>
2026-05-02 22:15:34 +0530

    Update DroidKit to 0.0.2

--- Casks/droidkit.rb
+++ Casks/droidkit.rb
@@ -1,6 +1,6 @@
 cask "droidkit" do
-  version "0.0.1"
-  sha256 "5c8bb8ddc6e623db3b64fce6ce397b19ae46532663616b12fae0e7bb230768f0"
+  version "0.0.2"
+  sha256 "7612b8445ba1020f78c5022dd293bebe622bb138be2da79f5a9e6b3608df4f05"
 
   url "https://github.com/dishant0406/droid/releases/download/v#{version}/Droid-#{version}-arm64.dmg"
   name "Droid"
